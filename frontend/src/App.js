fd6628aa4e3310e6eb5c6f8b6c769f74b9e00da5
leesooy01 <122393258+leesooy01@users.noreply.github.com>
2024-07-21 11:49:20 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,7 +3,10 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import Home from './pages/Home.js';
 import SignIn from './pages/SignIn.js';
-import SignUp from './pages/SignUp.js'
+import SignUp from './pages/SignUp.js';
+import UserProfile from './pages/UserProfile.js'
+
+
 
 
 function App() {
@@ -13,10 +16,10 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/UserProfile" element={<UserProfile />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
