2fe94960769d487aea779ccf7854684b5de70fb4
leesooy01 <122393258+leesooy01@users.noreply.github.com>
2024-07-21 11:49:20 -0500

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 // App.js
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/Home.js";
-import SignIn from "./pages/SignIn.js";
-import SignUp from "./pages/SignUp.js";
-import Dashboard from "./pages/Dashboard.js";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Home from './pages/Home.js';
+import SignIn from './pages/SignIn.js';
+import SignUp from './pages/SignUp.js';
+import UserProfile from './pages/UserProfile.js'
+
+
+
 
 function App() {
   return (
@@ -13,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/UserProfile" element={<UserProfile />} />
       </Routes>
     </Router>
   );
