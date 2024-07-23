de054f941f3aa8524952cfd96bb6e5e39e8117d8
leesooy01 <122393258+leesooy01@users.noreply.github.com>
2024-07-23 16:12:09 -0500

    Update Nav.js

--- frontend/src/pages/Nav.js
+++ frontend/src/pages/Nav.js
@@ -1,30 +1,36 @@
 import * as React from "react";
+import { Link } from 'react-router-dom';
 
 function Nav() {
   return (
     <div className="flex flex-col justify-center px-9 py-20 mx-auto w-full text-2xl font-black text-white bg-lime-200 border border-black border-solid max-w-[480px]">
       <div className="flex flex-col px-10 py-10 mt-6 bg-white">
-        <div className="self-center text-3xl italic text-center text-black">
+
+      <div className="self-center text-3xl italic text-center text-black">
           Home
         </div>
         <div className="self-center mt-5 text-3xl italic text-center text-black">
           Page
         </div>
-        <div className="p-2.5 mt-14 italic bg-black border border-black border-solid shadow-sm">
-          &nbsp; &nbsp;Workout Plan
-        </div>
-        <div className="p-2.5 mt-3 italic bg-black border border-black border-solid shadow-sm">
-          &nbsp; Workout Cards
-        </div>
-        <div className="p-2.5 mt-3 italic whitespace-nowrap bg-black border border-black border-solid shadow-sm">
+        <button className="w-full p-2.5 mt-14 italic bg-black text-white border border-black border-solid shadow-sm">
+          Workout Plan
+        </button>
+        <button className="w-full p-2.5 mt-3 italic bg-black text-white border border-black border-solid shadow-sm">
+          Workout Card
+        </button>
+        <button className="w-full p-2.5 mt-3 italic whitespace-nowrap bg-black text-white border border-black border-solid shadow-sm">
           Progress
-        </div>
-        <div className="py-2.5 pr-2.5 pl-2.5 mt-3.5 italic bg-black border border-black border-solid shadow-sm">
-          &nbsp; &nbsp; &nbsp; &nbsp; Settings
-        </div>
-        <div className="p-2.5 mt-3.5 italic whitespace-nowrap bg-black border border-black border-solid shadow-sm">
+        </button>
+        <Link to="/UserProfile">
+        <button className="w-full py-2.5 pr-2.5 pl-2.5 mt-3.5 italic bg-black text-white border border-black border-solid shadow-sm">
+          Settings
+        </button>
+        </Link>
+        <Link to="/">
+        <button className="w-full p-2.5 mt-3.5 italic whitespace-nowrap bg-black text-white border border-black border-solid shadow-sm">
           Logout
-        </div>
+        </button>
+        </Link>
         <img
           loading="lazy"
           src="/images/arm.jpg"
