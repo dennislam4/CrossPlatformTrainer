98a654fbc4163229b50da7a509c4304d718da684
Melanie Archer <job@twobanjos.com>
2024-07-25 11:13:53 -0700

    Weekly Workout List
    * controller method to create a weekly fitness plan, by fetching
      exercises filtered by the user survey, then grouping the exercises by
      `force`, then creating workout cards for each exercise. The exercises
      are saved to the Daily Workout List, and the 7 Daily Workout Lists are
      saved to the user's weekly Workout Plan.
    
      TODO:
      * program in rest days
      * add `skip` to workout card, which would remove from the daily
        workout and weekly workly lists

--- backend/weeklyFitnessPlanSchema.mjs
+++ backend/weeklyFitnessPlanSchema.mjs
@@ -1,16 +1,14 @@
 import mongoose from "mongoose";
 
 const weeklyFitnessPlanSchema = mongoose.Schema({
-  id: { type: String },
-  name: { type: String, required: true },
-  workout_1_id: [{ type: String }],
-  workout_2_id: [{ type: String }],
-  workout_3_id: [{ type: String }],
-  workout_4_id: [{ type: String }],
-  workout_5_id: [{ type: String }],
-  workout_6_id: [{ type: String }],
-  workout_7_id: [{ type: String }],
-  user_id: { type: String },
+  workout_1_id: { type: String },
+  workout_2_id: { type: String },
+  workout_3_id: { type: String },
+  workout_4_id: { type: String },
+  workout_5_id: { type: String },
+  workout_6_id: { type: String },
+  workout_7_id: { type: String },
+  user_id: { type: String, required: true },
 });
 
 const WeeklyFitnessPlan = mongoose.model(
