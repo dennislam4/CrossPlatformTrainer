cef961529726df7d963c5cea0a93f4717c5448a6
Dennis T Lam <107871794+dennislam4@users.noreply.github.com>
2024-07-30 18:37:46 -0600

    Create Sidebar.js and components folder
    Navigation sidebard for in app navigation between Dashboard, Fitness Plan, Daily Workouts, Progression, and User Profile

---
+++ frontend/src/components/Sidebar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Sidebar = () => {
+    return (
+        <div className="w-full bg-lime-200 text-black flex justify-around items-center border-t border-black border-solid h-16 fixed bottom-0">
+            <Link to="/Dashboard" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
+                Dashboard
+            </Link>
+            <Link to="/fitnessplan/:userId" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
+                Fitness Plan
+            </Link>
+            <Link to="/daily-workouts/:userId/:day" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
+                Daily Workouts
+            </Link>
+            <Link to="/Progression" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
+                Progression
+            </Link>
+            <Link to="/userprofile/:userId" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
+                User Profile
+            </Link>
+        </div>
+    );
+};
+
+export default Sidebar;
