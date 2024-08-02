953aa568f981f25461e5c8796d0724f66e76d5f1
Dennis T Lam <107871794+dennislam4@users.noreply.github.com>
2024-08-02 15:56:58 -0600

    Added Logout button to Sidebar.js

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const Sidebar = () => {
+const Sidebar = ({ handleLogout }) => {
     return (
         <div className="w-full bg-lime-200 text-black flex justify-around items-center border-t border-black border-solid h-16 fixed bottom-0">
             <Link to="/Dashboard" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
@@ -16,11 +16,11 @@ const Sidebar = () => {
             <Link to="/Progression" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
                 Progression
             </Link>
-            <Link to ="/workoutcard/:workoutCardId" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
-                Workout Cards
-            </Link>
             <Link to="/userprofile/:userId" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
                 User Profile
+            </Link>
+            <Link onClick={handleLogout} className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet--300">
+                Logout
             </Link>
         </div>
     );
