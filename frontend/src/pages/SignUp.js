765f7c02b1b077fa9b4dcd5fe90796aedebab966
Dennis T Lam <107871794+dennislam4@users.noreply.github.com>
2024-08-07 12:17:48 -0600

    Update SignUp.js to include API_URL

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -2,6 +2,8 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 function SignUp() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
