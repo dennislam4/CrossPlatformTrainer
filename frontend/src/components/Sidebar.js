c2f439337501ea6b9c615257aeeed9f89b4567cb
Dennis T Lam <107871794+dennislam4@users.noreply.github.com>
2024-08-08 17:12:13 -0600

    Define userId in Sidebar.js
    Pass through ${userId}

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -7,16 +7,16 @@ const Sidebar = ({ handleLogout }) => {
             <Link to="/Dashboard" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
                 Dashboard
             </Link>
-            <Link to="/fitnessplan/:userId" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
+            <Link to="/fitnessplan/${userId}" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
                 Fitness Plan
             </Link>
-            <Link to="/daily-workouts/:userId/:day" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
+            <Link to="/daily-workouts/${userId}/:day" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
                 Daily Workouts
             </Link>
             <Link to="/Progression" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
                 Progression
             </Link>
-            <Link to="/userprofile/:userId" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
+            <Link to="/userprofile/${userId}" className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet-300">
                 User Profile
             </Link>
             <button onClick={handleLogout} className="block px-4 py-2 italic font-bold text-black bg-white border border-black border-solid rounded hover:bg-violet--300">
